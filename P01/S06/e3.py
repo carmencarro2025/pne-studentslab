b18549caed0a91a20f025690ef10bc17dd11e9b8
carmencm <c.carro.2025@alumnos.urjc.es>
2026-02-24 16:55:10 +0100

    End of P00.

---
+++ P01/S06/e3.py
@@ -0,0 +1,52 @@
+class Seq:
+    """A class for representing sequences"""
+
+    def __init__(self, strbases):
+        # Initialize the sequence with the value
+        # passed as argument when creating the object
+        bases = "AGCT"
+        valid = True
+        for base in strbases:
+            if base not in bases:
+                valid = False
+        if valid:
+            self.strbases = strbases
+            print("New sequence created!")
+        else:
+            self.strbases = "ERROR"
+            print("ERROR!!")
+
+    def __str__(self):
+        """Method called when the object is being printed"""
+        # -- We just return the string with the sequence
+        return self.strbases
+
+    def len(self):
+        """Calculate the length of the sequence"""
+        return len(self.strbases)
+
+def print_seqs(seq_list):
+    i = 1
+    for seq in seq_list:
+        print(f"Sequence {i}: (Length:{ seq.len()}) {seq}")
+        i += 1
+
+def generate_seqs(pattern, number):
+    lst = []
+    i = 0
+    seq = ""
+    while i < number:
+        seq += pattern
+        lst.append(Seq(seq))
+        i += 1
+    return lst
+
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+
+print("List 1:")
+print_seqs(seq_list1)
+
+print()
+print("List 2:")
+print_seqs(seq_list2)
