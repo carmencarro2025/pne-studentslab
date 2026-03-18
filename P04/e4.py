2d92b4ad5da7ead25acdba77d5dc8f961199d095
carmencm <c.carro.2025@alumnos.urjc.es>
2026-03-18 18:40:53 +0100

    Exercises finished.

---
+++ P04/e4.py
@@ -0,0 +1,95 @@
+import socket
+import termcolor
+from pathlib import Path
+
+
+# -- Server network parameters
+IP = "127.0.0.1"
+PORT = 8080
+
+
+def process_client(s):
+    # -- Receive the request message
+    req_raw = s.recv(2000)
+    req = req_raw.decode()
+
+    print("Message FROM CLIENT: ")
+
+    # -- Split the request messages into lines
+    lines = req.split('\n')
+
+    # -- The request line is the first
+    req_line = lines[0]
+    get_d = req_line.split(" ")
+    if len(get_d) > 1:
+        if get_d[1].startswith("/info/"):
+            bases = ["A", "G", "C", "T"]
+            print(get_d[-1])
+            if (get_d[1])[-1] in bases:
+                body = Path(f"html/info/{(get_d[1])[-1]}.html").read_text()
+
+        else:
+            body = """
+            <!DOCTYPE html>
+            <html lang="en" dir="ltr">
+              <head>
+                <meta charset="utf-8">
+                <title>Blank body</title>
+              </head>
+              <body style="background-color: white;">
+                <h1>NOT VALID</h1>
+              </body>
+            </html>
+            """
+
+
+    print("Request line: ", end="")
+    termcolor.cprint(req_line, "green")
+
+
+    # -- Status line: We respond that everything is ok (200 code)
+    status_line = "HTTP/1.1 200 OK\n"
+
+    # -- Add the Content-Type header
+    header = "Content-Type: text/html\n"
+
+    # -- Add the Content-Length
+    header += f"Content-Length: {len(body)}\n"
+
+    # -- Build the message by joining together all the parts
+    response_msg = status_line + header + "\n" + body
+    cs.send(response_msg.encode())
+
+
+# -------------- MAIN PROGRAM
+# ------ Configure the server
+# -- Listening socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Setup up the socket's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Become a listening socket
+ls.listen()
+
+print("Green server configured!")
+
+# --- MAIN LOOP
+while True:
+    print("Waiting for clients....")
+    try:
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server stopped!")
+        ls.close()
+        exit()
+    else:
+
+        # Service the client
+        process_client(cs)
+
+        # -- Close the socket
+        cs.close()
